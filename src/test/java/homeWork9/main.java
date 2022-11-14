35d5e022e358c1fcf10b36129a0a4cf26a70dcd8
Nazar.Nerubayko <nazarka614@gmail.com>
2022-11-15 00:26:03 +0200

    -First test

--- src/test/java/homeWork9/main.java
+++ src/test/java/homeWork9/main.java
@@ -1,11 +1,14 @@
 package homeWork9;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class main {
+    static WebDriver driver = new ChromeDriver();
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        firstTest();
+
+    }
+    public static void firstTest(){
         driver.manage().window().maximize();
         driver.get("https://ithillel.ua/");
         System.out.println(driver.getTitle());
