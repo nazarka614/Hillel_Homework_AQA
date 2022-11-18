874452d11d1f03fed009727aa689826800b7b784
Nazar.Nerubayko <nazarka614@gmail.com>
2022-11-18 17:52:58 +0200

    - Homework10

--- src/test/java/homeWork10/Main.java
+++ src/test/java/homeWork10/Main.java
@@ -2,8 +2,8 @@ package homeWork10;
 
 public class Main extends BaseClass{
     public static void main(String[] args) {
-        String string = "https://demoqa.com/automation-practice-form";
         BaseClass.createDriver();
+        String string = "https://demoqa.com/automation-practice-form";
         BaseClass.setURL(string);
         BaseClass.FillDataInFields();
         BaseClass.closeChrome();
